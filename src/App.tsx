37a156b96a1756ef54941f76c647487483f80ed4
alexrich123 <alexchristopherrichards@hotmail.com>
2022-04-10 09:17:20 -0400

    navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import About from './pages/About';
 import Navbar from './components/navbar/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app/App.css';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@blueprintjs/core'
 import '../../styles/navbar/Navbar.css';
+
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <div className="col-lg-4 logo">
-        <h1 className="navLink">Hello</h1>
+      <div className="col-lg-4 navStart">
+        <h1 className="logo">Logo Here</h1>
       </div>
-      <div className="col-lg-4 navLink">
-        <Link to="/"> Home </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/courses"> Courses </Link>
+      <div className="col-lg-4 navCenter">
+        <Link to="/" className='navLink'> Home </Link>
+        <Link to="/about" className='navLink'> About </Link>
+        <Link to="/courses"className='navLink'> Courses </Link>
       </div>
-      <div className="col-lg-4">
-        <Link to='/'>Login</Link>
+      <div className="col-lg-4 navEnd">
+        <Button intent='primary'>Login</Button>
       </div>
     </div>
   );
