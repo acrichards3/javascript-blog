745935f2c7eaf0b15b36da0332e7661159fd59e3
alexrich123 <alexchristopherrichards@hotmail.com>
2022-04-10 19:07:49 -0400

    scss modules

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from '@blueprintjs/core'
-import '../../styles/navbar/Navbar.scss';
+import styles from '../../styles/navbar/Navbar.module.scss';
 
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <div className="col-lg-4 navStart">
-        <h1 className="logo">Logo Here</h1>
+    <div className={styles.navbar}>
+      <div className={styles.navStart}>
+        <h1 className={styles.logo}>Logo Here</h1>
       </div>
-      <div className="col-lg-4 navCenter">
-        <Link to="/" className="navLink">
+      <div className={styles.navCenter}>
+        <Link to="/" className={styles.navLink}>
           <Icon icon="home" color="#99A5B9" /> Home{' '}
         </Link>
-        <Link to="/blog" className="navLink">
+        <Link to="/blog" className={styles.navLink}>
           <Icon icon="manually-entered-data" color="#99A5B9" /> Blog{' '}
         </Link>
-        <Link to="/courses" className="navLink">
+        <Link to="/courses" className={styles.navLink}>
           <Icon icon="learning" color="#99A5B9" /> Courses{' '}
         </Link>
       </div>
-      <div className="col-lg-4 navEnd">
-        <button className="loginBtn">Sign In</button>
+      <div className={styles.navEnd}>
+        <button className={styles.loginBtn}>Sign In</button>
       </div>
     </div>
   );
