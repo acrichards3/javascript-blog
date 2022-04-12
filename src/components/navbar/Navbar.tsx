aea4ea1f8eeb7907a5400837bfe643d852d75f02
alexrich123 <alexchristopherrichards@gmail.com>
2022-04-12 04:28:47 -0400

    mobile nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from '@blueprintjs/core';
-import MobileNav from './MobileNav';
 import styles from '../../styles/navbar/Navbar.module.scss';
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleOpen = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
     <div className={styles.navbar}>
       <div className={styles.navStart}>
         <Link to="/" style={{ textDecoration: 'none' }}>
           <h1 className={styles.logo}>Logo Here</h1>
         </Link>
-          <Icon onClick={toggleOpen} icon="menu" color="#99a5b9" className={styles.mobileBtn} />
-          <div className={isOpen ? styles.mobileOpen : styles.mobileClosed}>
-            <MobileNav />
-          </div>
       </div>
       <div className={styles.navCenter}>
+        {/* MOBILE VIEW */}
+        <Link to="/" className={styles.homeIconMobile}>
+          <Icon icon="home" size={25} color="#99a5b9" />
+        </Link>
+        <Link to="/blog" className={styles.blogIconMobile}>
+          <Icon icon="manually-entered-data" size={25} color="#99a5b9" />
+        </Link>
+        <Link to="/courses" className={styles.coursesIconMobile}>
+          <Icon icon="learning" size={25} color="#99a5b9" />
+        </Link>
+        {/* DESKTOP VIEW */}
         <div className={styles.homeIcon}>
           <Link to="/" className={styles.homeIcon}>
             <Icon icon="home" /> Home{' '}
