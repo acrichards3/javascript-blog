31f96ffb6af526437d351ce5b664e7ac76e349e0
alexrich123 <alexchristopherrichards@gmail.com>
2022-04-12 22:14:58 -0400

    nav

---
+++ src/components/navbar/Login.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Dialog } from '@blueprintjs/core';
+
+export default function Login() {
+    const [isOpen, setIsOpen] = useState(false);
+
+
+  return (
+    <div>Login</div>
+  )
+}
